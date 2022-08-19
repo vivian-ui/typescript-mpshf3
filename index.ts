90dcf92d0f733b7b148d11749fd01de0f28e057f
vivian-ui <vi_cunha@hotmail.com>
2022-08-19 20:06:42 -0300

    contador

--- index.ts
+++ index.ts
@@ -3,14 +3,10 @@ import './style.css';
 
 // Write TypeScript code!
 let title: string = 'My App';
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>${title}</h1>
-<hr>
-<p>paragrafo</p>
-`;
-console.log('Ola mundo');
-for (let i = 0; i <= 10; i++) {
-  if (i % 2 == 0) {
-    console.log(i);
-  }
-}
+document.getElementById('app.title').innerHTML = title;
+let counter: number = 0;
+let intervalId = setInterval(() => {
+  counter = counter + 1;
+  let counterDiv = document.getElementById('app.counter');
+  counterDiv.innerHTML = counter.toString();
+}, 10000);
