cccc2d2c82147f7b1d3e552c932f5033d8c2a566
vivian-ui <vi_cunha@hotmail.com>
2022-08-19 20:39:19 -0300

    tentativa exercicio 01 - tabata treino

--- index.ts
+++ index.ts
@@ -5,8 +5,11 @@ import './style.css';
 let title: string = 'My App';
 document.getElementById('app.title').innerHTML = title;
 let counter: number = 0;
-let intervalId = setInterval(() => {
-  counter = counter + 1;
-  let counterDiv = document.getElementById('app.counter');
-  counterDiv.innerHTML = counter.toString();
-}, 10000);
+
+for (let i = 0; i <= 8; i++) {
+  let intervalId = setInterval(() => {
+    counter = counter + 1;
+    let counterDiv = document.getElementById('app.counter');
+    counterDiv.innerHTML = counter.toString();
+  }, 10000);
+}
