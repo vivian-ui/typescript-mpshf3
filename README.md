4e2e8f667adeaa1286431bb0d6c86365f9789147
vivian-ui <vi_cunha@hotmail.com>
2022-08-19 19:31:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-mpshf3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-mpshf3)
---
+++ index.ts
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+console.log('Ola mundo');
+for (let i = 0; i <= 10; i++) [console.log(i)];
