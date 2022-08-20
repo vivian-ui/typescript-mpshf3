b798dd7ad0724780d4773ae5d90c570ae48c5f18
vivian-ui <vi_cunha@hotmail.com>
2022-08-19 21:47:32 -0300

    tabata treino

--- index.ts
+++ index.ts
@@ -13,15 +13,16 @@ let intervalId = setInterval(() => {
 
   if (counter == 20) {
     counter = 0;
-    ciclo = ciclo + 1;
-  }
-  if (ciclo == 8) {
-    ciclo = 0;
     descanso = descanso + 1;
   }
   if (descanso == 10) {
     descanso = 0;
+    ciclo = ciclo + 1;
   }
+  if (ciclo == 8) {
+    ciclo = 0;
+  }
+
   let counterDiv = document.getElementById('app.counter');
   counterDiv.innerHTML = counter.toString();
-}, 10000);
+}, 100);
